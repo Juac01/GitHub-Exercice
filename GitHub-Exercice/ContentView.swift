48ec9c6454b35044e537d8836b6854e27bc8e647
Juac01 <julcamoro1998@hotmail.com>
2024-09-20 17:04:24 -0300

    Initial Commit

---
+++ GitHub-Exercice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub-Exercice
+//
+//  Created by JOSE JOAQUIN JULCAMORO BUSTAMANTE on 20/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub-Exercice/GitHub_ExerciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciceApp.swift
+//  GitHub-Exercice
+//
+//  Created by JOSE JOAQUIN JULCAMORO BUSTAMANTE on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
